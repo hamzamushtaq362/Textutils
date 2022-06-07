4026e2bfa0857b5cd6a815151ef15e553fa0a024
hamzamushtaq362 <hamzamushtaq362@gmail.com>
2022-06-07 23:55:34 +0500

    Updates

---
+++ precache-manifest.118af8836f5358568ff372ec395d4202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ae358351ea497cb5e9",
+    "url": "/Textutils/static/css/main.728e84ee.chunk.css"
+  },
+  {
+    "revision": "62ae358351ea497cb5e9",
+    "url": "/Textutils/static/js/main.62ae3583.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/Textutils/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/Textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "4f3474d073f9e1fe319c",
+    "url": "/Textutils/static/js/runtime~main.4f3474d0.js"
+  },
+  {
+    "revision": "7f3f6c77e689d9f708a7c4b578832fb9",
+    "url": "/Textutils/index.html"
+  }
+];
